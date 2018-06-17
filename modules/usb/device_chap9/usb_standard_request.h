4eb32e85961150cb22520e10cba28e1e375b5008
Igor Liferenko <igor.liferenko@gmail.com>
2018-06-17 17:45:21 +0700

    edit

--- modules/usb/device_chap9/usb_standard_request.h
+++ modules/usb/device_chap9/usb_standard_request.h
@@ -1,40 +1,8 @@
-/*This file has been prepared for Doxygen automatic documentation generation.*/
-//! \file *********************************************************************
-//!
-//! \brief @briefProcess USB device enumeration requests header file.
-//!
-//!  This file contains the USB endpoint 0 management routines corresponding to
-//!  the standard enumeration process (refer to chapter 9 of the USB
-//!  specification.
-//!  This file calls routines of the usb_specific_request.c file for non-standard
-//!  request management.
-//!  The enumeration parameters (descriptor tables) are contained in the
-//!  usb_descriptors.c file.
-//!
-//! - Compiler:           IAR EWAVR and GNU GCC for AVR
-//! - Supported devices:  ATmega32U4
-//!
-//! \author               Atmel Corporation: http://www.atmel.com \n
-//!                       Support and FAQ: http://support.atmel.no/
-//!
-//! ***************************************************************************
-
 #ifndef _USB_STANDARD_REQUEST_H_
 #define _USB_STANDARD_REQUEST_H_
 
-//_____ I N C L U D E S ____________________________________________________
-
 #include "modules/usb/usb_task.h"
 #include "usb_descriptors.h"
-
-//_____ M A C R O S ________________________________________________________
-
-//_____ S T A N D A R D    D E F I N I T I O N S ___________________________
-
-//! @defgroup std_request USB device standard  requests decoding
-//! @{
-
-
 
         // Device State
 #define ATTACHED                          0
@@ -44,40 +12,12 @@
 #define CONFIGURED                        4
 #define SUSPENDED                         5
 
-//_____ D E C L A R A T I O N ______________________________________________
-
    //! @brief Returns true when device connected and correctly enumerated with an host.
    //! The device high level application should tests this before performing any applicative requests
 #define Is_device_enumerated()            ((usb_configuration_nb!=0)   ? TRUE : FALSE)
 #define Is_device_not_enumerated()        ((usb_configuration_nb!=0)   ? FALSE : TRUE)
 
-
-
-
-//! @brief This function reads the SETUP request sent to the default control endpoint
-//! and calls the appropriate function. When exiting of the usb_read_request
-//! function, the device is ready to manage the next request.
-//!
-//! If the received request is not supported or a none USB standard request, the function
-//! will call for custom decoding function in usb_specific_request module.
-//!
-//! @param none
-//!
-//! @return none
-//! @note list of supported requests:
-//! SETUP_GET_DESCRIPTOR
-//! SETUP_GET_CONFIGURATION
-//! SETUP_SET_ADDRESS
-//! SETUP_SET_CONFIGURATION
-//! SETUP_CLEAR_FEATURE
-//! SETUP_SET_FEATURE
-//! SETUP_GET_STATUS
-//!
-void    usb_process_request( void);
-
 extern  U8   usb_configuration_nb;
-
-//! @}
 
 #endif  // _USB_STANDARD_REQUEST_H_
 
