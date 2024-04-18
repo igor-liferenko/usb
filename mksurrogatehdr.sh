58e2a6105076fc73e3aadca59fff33effeee9024
Igor Liferenko <igor.liferenko@gmail.com>
2024-04-18 20:36:47 +0700

    cleanup

--- mksurrogatehdr.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Taken from https://stackoverflow.com/questions/20889460/
-sysdir="./system-headers"
-for header in "$@"
-do
-    mkdir -p "$sysdir/$(dirname $header)"
-    echo "include <$header>" > "$sysdir/$header"
-done
